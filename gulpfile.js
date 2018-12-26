a4a886eacbe00992d49da6370d7373e9761ed8f0
joao <joao.paulo@mobilesaude.com.br>
2018-12-25 22:44:45 -0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
-// import { gulp } from 'gulp';
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
 
-gulp.task('minify-images', function() {
-    gulp.src('assets/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('assets/images'));
-});
+// gulp.task('minify-images', function() {
+//     gulp.src('assets/images/**/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('assets/images'));
+// });
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
